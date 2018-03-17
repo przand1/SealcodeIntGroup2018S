85cde2aa3a36e63962a0c169efda8daa544eafa3
przand1 <przand1@st.amu.edu.pl>
2018-03-17 11:37:14 +0100

    zaj2

---
+++ Zaj1/kontakt.php
@@ -0,0 +1,37 @@
+<?php
+
+// Tworzymy zmienną dla imienia i nazwiska
+$name = $_POST['name'];
+
+// Tworzymy zmienną dla adresu email
+$email = $_POST['email'];
+
+// Tworzymy zmienną dla wiadomości
+$message = $_POST['message'];
+
+// Podajesz adres email z którego ma być wysłana wiadomość
+$odkogo = "pand5@wp.pl";
+
+// Podajesz adres email na który chcesz otrzymać wiadomość
+$dokogo = "przand1@st.amu.edu.pl";
+
+// Podajesz tytuł jaki ma mieć ta wiadomość email
+$tytul = "Wiadomość z pracy domowej";
+
+// Przygotowujesz treść wiadomości
+$wiadomosc = "";
+$wiadomosc .= "Imie i nazwisko: " . $name . "\n";
+$wiadomosc .= "Email: " . $email . "\n";
+$wiadomosc .= "Wiadomość: " . $message . "\n";
+
+// Wysyłamy wiadomość
+$sukces = mail($dokogo, $tytul, $wiadomosc, "Od: <$odkogo>");
+
+// Przekierowywujemy na potwierdzenie
+if ($sukces){
+  print "<meta http-equiv=\"refresh\" content=\"0;URL=potwierdzenie.php\">";
+}
+else{
+  print "<meta http-equiv=\"refresh\" content=\"0;URL=error.html\">";
+}
+?>
